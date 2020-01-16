cdda5db74f5751b2d2f0d42fe0a95454462e7acc
Michelle Ranly <meranly8@gmail.com>
2020-01-16 17:37:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(game_board)
+  puts " #{game_board[0]} | #{game_board[1]} | #{game_board[2]} "
+  puts "-----------"
+  puts " #{game_board[3]} | #{game_board[4]} | #{game_board[5]} "
