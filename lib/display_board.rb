ddab678a60b47d5aaa80f23d7fd9ea5fbedc13d9
Michelle Ranly <meranly8@gmail.com>
2020-01-16 17:38:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,10 @@ def display_board(game_board)
   puts " #{game_board[0]} | #{game_board[1]} | #{game_board[2]} "
   puts "-----------"
   puts " #{game_board[3]} | #{game_board[4]} | #{game_board[5]} "
+  puts "-----------"
+  puts " #{game_board[6]} | #{game_board[7]} | #{game_board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+display_board(board)
